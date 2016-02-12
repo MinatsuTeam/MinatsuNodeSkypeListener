903b378c699a718d2b7e136511ac0d924b80632f
tryy3 <dennistryy3@gmail.com>
2016-02-13 00:54:32 +0100

    Preparing to support more standard listener types, like sending some data on connection, color support, arrays in sendMessage and so on.

--- net.js
+++ net.js
@@ -22,65 +22,88 @@ var timeout = 0;
 var client = new net.Socket();
 
 var connect = function() {
-	if (timeout > 0) {
-		console.log("Trying to connect in " + timeout + " seconds.");
-	}
+    if (timeout > 0) {
+        console.log("Trying to connect in " + timeout + " seconds.");
+    }
 
-	setTimeout(function() {
-		client.connect(PORT, HOST, function() {
-			console.log("Connected");
-			timeout = 0;
-		});
-	}, timeout * 1000);
-	timeout = (timeout <= 60) ? timeout+10 : timeout;
+    setTimeout(function() {
+        client.connect(PORT, HOST, function() {
+            console.log("Connected");
+
+            client.write({
+                "Name": "Node Skype",
+                "Type": "Skype",
+                "VersionStandard": "0.0.1",
+                "VersionListener": "0.0.1"
+            });
+
+            timeout = 0;
+      });
+    }, timeout * 1000);
+    timeout = (timeout <= 60) ? timeout+10 : timeout;
+}
+var sendMessages = function(data) {
+    if (!Array.isArray(data)) sendMessage(data);
+
+    for (var key in data) {
+        if (data.hasOwnProperty(key)) {
+            continue;
+        }
+        sendMessage(data[key]);
+    }
 }
 
-var sendMessage = function(Message) {
-	if (Message.length < 2) {
-        console.log("The arguments is invalid.")
-        console.log('DATA ' + client.remoteAddress + ': ' + Message);
+var sendMessage = function(data) {
+    if (!(data instanceof Object)) {
+        console.log("The data is not a object.")
+        console.log("DATA " + client.remoteAddress + ": " + data);
         return;
-	}
+    }
 
-    skyweb.sendMessage(Message[0], Message[1]);
+    data.message = data.message.replace(new RegExp("(&[0-9|a-f])|({(dark_)?(blue|green|aqua|red|purple|gray|black|yellow|gold|white)})", 'g'), "");
+    
+    skyweb.sendMessage(data.channel, data.message);
 }
 
 connect();
 
 client.on('error', function() {
-	console.log("Can't connect, trying again.");
-	connect();
+    console.log("Can't connect, trying again.");
+    connect();
 })
 
 client.on("data", function(data) {
-	console.log("Received: " + data);
-	var dataJson = JSON.parse(data.toString());
+    console.log("Received: " + data);
+    var dataJson = JSON.parse(data.toString());
 
-	if (dataJson.length < 2) {
-		console.log("The data does not have enough arguments.");
-		console.log("DATA " + client.remoteAddress + ": " + data);
-		return;
-	}
+    if (!(dataJson instanceof Object)) {
+        console.log("The data is not a object.");
+        console.log("DATA " + client.remoteAddress + ": " + dataJson);
+        return;
+    }
 
-	if (dataJson[0] == "sendMessage") {
-		for (var i = 0; i < dataJson[1].length; i++) {
-			sendMessage(dataJson[1][i]);
-		}
-
-	}
+    if (dataJson.type == "sendMessage") {
+        sendMessages(dataJson.data);
+    }
 });
 
 client.on("close", function() {
-	console.log("Connection closed.");
+    console.log("Connection closed.");
 })
 
 skyweb.messagesCallback = function(messages) {
-	messages.forEach(function(message) {
-		if (message.resource.from.indexOf("tryy3.bot")=== -1 && message.resource.messagetype !== 'Control/Typing' && message.resource.messagetype !== 'Control/ClearTyping') {
-			var link = message.resource.conversationLink;
+    messages.forEach(function(message) {
+        if (message.resource.from.indexOf("tryy3.bot")=== -1 && message.resource.messagetype !== 'Control/Typing' && message.resource.messagetype !== 'Control/ClearTyping') {
+            var link = message.resource.conversationLink;
             var id = link.substring(link.lastIndexOf("/") + 1);
             var from = message.resource.from.substring(message.resource.from.lastIndexOf('/') + 1);
-            client.write(JSON.stringify(["onMessageEvent", [id, from, message.resource.content]]) + "\n");
-		}
-	})
+            client.write(JSON.stringify({
+                "type": "onMessageEvent",
+                "data": {
+                    "id" : id,
+                    "from" : from,
+                    "message" : message.resource.content
+                }}) + "\n");
+        }
+    })
 };
